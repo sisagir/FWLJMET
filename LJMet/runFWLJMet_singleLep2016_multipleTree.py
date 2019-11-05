be94029fb66b2f3bba3f77b5441f5b94e6f884f5
Emanuele Usai <ema2487@gmail.com>
2019-11-05 14:36:32 -0600

    uncomment required fields in the WeightAnalyzer

--- LJMet/runFWLJMet_singleLep2016_multipleTree.py
+++ LJMet/runFWLJMet_singleLep2016_multipleTree.py
@@ -105,8 +105,8 @@ process.TFileService = cms.Service("TFileService", fileName = cms.string(OUTFILE
 process.mcweightanalyzer = cms.EDAnalyzer(
     "WeightAnalyzer",
     overrideLHEweight = cms.bool(False), # 2016 is NNPDF30
-    #basePDFname = cms.string("NNPDF31_nnlo_as_0118_nf_4"),
-    #newPDFname = cms.string("NNPDF31_nnlo_as_0118_nf_4_mc_hessian"),
+    basePDFname = cms.string("NNPDF31_nnlo_as_0118_nf_4"),
+    newPDFname = cms.string("NNPDF31_nnlo_as_0118_nf_4_mc_hessian"),
     )
 
 ################################
